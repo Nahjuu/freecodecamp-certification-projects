5300e2a6117fe7aaeb13e0201152be286c33e0ed
Nahuel Silva <nahuelsilvanssi@gmail.com>
2026-09-01 15:03:48 +0000

    Implement sound playback for drum machine
    Added functionality to play sounds on button click and keyboard press.

--- javascript/drum-machine/script.js
+++ javascript/drum-machine/script.js
@@ -1,1 +1,23 @@
-nothin yet
+let soundButtons = document.querySelectorAll(".drum-pad");
+let clips = document.querySelectorAll(".clip");
+let display = document.getElementById("display");
+
+//function that makes a sound when i click a button.
+function playButtonSound(event) {
+  let button = event.currentTarget;
+  let audio = button.querySelector(".clip");
+  display.innerText = button.name
+  audio.play();
+}
+//Pressing button eventListener
+soundButtons.forEach(soundButton => soundButton.addEventListener("click", playButtonSound));
+
+//function that makes a sound when i press a keyboard key.
+function keyboardSound(event){
+  let audio = document.getElementById(event.key.toUpperCase());
+  audio.play()
+
+}
+
+//keydown event listener
+document.addEventListener("keydown",keyboardSound )
